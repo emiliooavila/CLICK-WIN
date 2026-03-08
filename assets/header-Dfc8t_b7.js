11f53261f2c90f44dcf24169e377747c95fa284f
emiliooavila <avilavaladezjorgeemilio1010@gmail.com>
2026-03-08 17:14:09 -0600

    Updates

--- assets/header-Dfc8t_b7.js
+++ assets/header-Dfc8t_b7.js
@@ -1,8 +1,8 @@
-function d({step:s,total:a,progress:r}){const e=document.getElementById("appHeader");e&&(e.innerHTML=`
+import{B as i}from"./storage-CiY_4-N5.js";function n({step:s,total:a,progress:r}){const e=document.getElementById("appHeader");e&&(e.innerHTML=`
     <header class="app-header">
       <div class="inner">
         <div class="brand">
-          <img src="/CLICK&WIN-LOGO-HEADER.jpg" alt="Click & Win" class="brand-logo" />
+          <img src="${i}clickwin-logo-header.jpg" alt="Click & Win" class="brand-logo" />
         </div>
         <span class="step-indicator">Paso ${s} de ${a}</span>
       </div>
@@ -10,4 +10,4 @@ function d({step:s,total:a,progress:r}){const e=document.getElementById("appHead
         <div class="progress-fill" style="width:${r}%"></div>
       </div>
     </header>
-  `)}export{d as r};
+  `)}export{n as r};
